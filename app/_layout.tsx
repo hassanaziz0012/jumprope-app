d609c375ec745182cff3cdd73f7f66c050632a07
Hassan Aziz <hassanaziz0012@gmail.com>
2026-03-07 17:16:59 +0500

    feat: wire up notifications in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,12 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import { initDatabase, getUserProfile } from "../lib/database";
+import { scheduleStreakNotification } from "../lib/notifications";
 import { SyncToast } from "./components/SyncToast";
 import * as Sentry from '@sentry/react-native';
 import { Platform } from 'react-native';
 import * as NavigationBar from 'expo-navigation-bar';
+
 
 Sentry.init({
   dsn: 'https://375656c4b25f5166691c77cc9968565a@o4510460628828160.ingest.de.sentry.io/4510634224648272',
@@ -33,6 +35,7 @@ const CustomDarkTheme = {
 export default Sentry.wrap(function RootLayout() {
     useEffect(() => {
         initDatabase();
+        scheduleStreakNotification();
         if (Platform.OS === 'android') {
             NavigationBar.setPositionAsync('absolute');
             NavigationBar.setVisibilityAsync('hidden');
@@ -81,6 +84,12 @@ export default Sentry.wrap(function RootLayout() {
                     />
                     <Stack.Screen
                         name="charts"
+                        options={{
+                            headerShown: false,
+                        }}
+                    />
+                    <Stack.Screen
+                        name="notifications"
                         options={{
                             headerShown: false,
                         }}
